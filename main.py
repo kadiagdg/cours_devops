231544f34d89d1e5984991e078b21166a9e308a1
azizdupont1 <armelle.kadia@gmail.com>
2025-06-12 21:50:41 +0000

    new update

--- main.py
+++ main.py
@@ -116,4 +116,4 @@ async def delete_item(item_id: int, db: Session = Depends(get_db)):
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=8001)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
